3086d6b72adb27a197406dc053b958511088ac73
Gabriel Nogueira <gnogueira.dev@gmail.com>
2017-05-29 14:34:20 -0300

    Initial commit

---
+++ src/com/nogueiragabriel/crawler/SpiderLeg.java
@@ -0,0 +1,89 @@
+package com.nogueiragabriel.crawler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SpiderLeg
+{
+    // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
+    private static final String USER_AGENT =
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    private List<String> links = new LinkedList<String>();
+    private Document htmlDocument;
+
+    public boolean crawl(String url)  {
+        try  {
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            
+            this.htmlDocument = htmlDocument;
+            if(connection.response().statusCode() == 200) {
+                System.out.println("\n**Visiting** Received web page at " + url);
+            }
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            for(Element link : linksOnPage) {
+                if(!link.absUrl("href").contains("#") 
+                	&& !link.absUrl("href").contains("$")
+                	&& !link.absUrl("href").contains("/entrar")
+                	&& link.absUrl("href").contains("ilocal"))
+                	this.links.add(link.absUrl("href"));
+            }
+
+            System.out.println("Found (" + linksOnPage.size() + ") links on page");
+            
+            /*
+            if(connection.response().contentType() != null){
+	            if(!connection.response().contentType().contains("text/html")) {
+	                System.out.println("**Failure** Retrieved something other than HTML");
+	                return false;
+	            }
+            }*/
+	            
+            String tel = htmlDocument.select("span[class=first-numbers]").first().text() 
+            			+ htmlDocument.select("span[class=first-numbers]").first().attr("data-last");
+            String adress = htmlDocument.select("div.footerMap>h3").text();
+            String name = htmlDocument.select("div.centralizePage>h1").text();
+            
+            System.out.println("Company name: " + name );
+            System.out.println("Found tel number: " + tel);
+            System.out.println("Company adress: " + adress);
+            
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+
+    /**
+     * Performs a search on the body of on the HTML document that is retrieved. This method should
+     * only be called after a successful crawl.
+     * 
+     * @param searchWord
+     *            - The word or string to look for
+     * @return whether or not the word was found
+     */
+    /*public boolean searchForWord(String searchWord) {
+        if(this.htmlDocument == null) {
+            System.out.println("ERROR! Call crawl() before performing analysis on the document");
+            return false;
+        }
+        
+        System.out.println("Searching for the word " + searchWord + "...");
+        String bodyText = this.htmlDocument.body().text();
+        return bodyText.toLowerCase().contains(searchWord.toLowerCase());
+    }*/
+
+    public List<String> getLinks() {
+        return this.links;
+    }
+
+}
